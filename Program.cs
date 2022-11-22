fd8eeba532964b434a5412ef5147f5440da6e4ec
sontvh <sontvh@bkhightech.vn>
2022-11-22 13:52:03 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    public class BenchmarkAPIPerformance
+    {
+        private static HttpClient _httpClient;
+
+        [Params(1)]
+        public int N;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:44336/api/app");
+            _httpClient.DefaultRequestHeaders.Add("accept", "application/json");
+        }
+
+        [Benchmark]
+        public async Task GetListFromRepo()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                var response = await _httpClient.GetAsync("/register-of-examination/from-repo");
+            }
+        }
+
+        [Benchmark]
+        public async Task GetListThenOrderBy()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                var response = await _httpClient.GetAsync("/register-of-examination/then-order-by");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BenchMarkDotNet");
+            var summary = BenchmarkRunner.Run<BenchmarkAPIPerformance>();
+        }
+    }
+}
